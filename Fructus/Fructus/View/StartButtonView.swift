22adf28431142f434d31abae2a347874ee0fd6e4
Clément Causer <ccauser@simplonprod.co>
2022-05-04 14:07:00 +0200

    add fruits list screen

--- Fructus/Fructus/View/StartButtonView.swift
+++ Fructus/Fructus/View/StartButtonView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct StartButtonView: View {
     // MARK: - PROPERTIES
     @AppStorage("isOnboarding") var isOnboarding: Bool?
-    
+
     // MARK: - BODY
     var body: some View {
         Button(action: {
